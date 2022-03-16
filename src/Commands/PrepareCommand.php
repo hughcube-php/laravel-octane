c55098a03b3b70f8b07ba039013ea0e7121d775b
hugh.li <hugh.li@foxmail.com>
2022-03-16 09:27:08 +0800

    feat: add state-file option

--- src/Commands/PrepareCommand.php
+++ src/Commands/PrepareCommand.php
@@ -12,6 +12,7 @@ use Laravel\Octane\Commands\StartSwooleCommand;
 use Laravel\Octane\Swoole\ServerProcessInspector;
 use Laravel\Octane\Swoole\ServerStateFile;
 use Laravel\Octane\Swoole\SwooleExtension;
+use ReflectionClass;
 
 class PrepareCommand extends StartSwooleCommand
 {
@@ -26,7 +27,8 @@ class PrepareCommand extends StartSwooleCommand
                     {--workers=auto : The number of workers that should be available to handle requests}
                     {--task-workers=auto : The number of task workers that should be available to handle tasks}
                     {--max-requests=500 : The number of requests to process before reloading the server}
-                    {--watch : Automatically reload the server when the application is modified}';
+                    {--watch : Automatically reload the server when the application is modified}
+                    {--state-file : Server state file}';
 
     /**
      * The command's description.
@@ -35,12 +37,31 @@ class PrepareCommand extends StartSwooleCommand
      */
     public $description = 'Prepare the Octane Swoole server';
 
+    /**
+     * @inheritdoc
+     */
     public function handle(
         ServerProcessInspector $inspector,
         ServerStateFile $serverStateFile,
         SwooleExtension $extension
-    ) {
+    ): int {
+        if (!empty($stateFile = $this->option('state-file'))) {
+            $this->setServerStateFilePath($serverStateFile, $stateFile);
+        }
+
         $this->writeServerStateFile($serverStateFile, $extension);
         $this->info(sprintf('file writing succeeded: %s', $serverStateFile->path()));
+
+        return 0;
+    }
+
+    protected function setServerStateFilePath(ServerStateFile $serverStateFile, $file)
+    {
+        $reflection = new ReflectionClass($serverStateFile);
+
+        $property = $reflection->getProperty('path');
+        $property->setAccessible(true);
+
+        $property->setValue($serverStateFile, $file);
     }
 }
