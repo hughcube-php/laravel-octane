abe86b857913ef1783ba08988c9bd423d6a156b6
hugh.li <hugh.li@foxmail.com>
2022-03-31 23:58:22 +0800

    feat: 剥离laravel-knight依赖

--- src/Actions/WaitTaskCompleteAction.php
+++ src/Actions/WaitTaskCompleteAction.php
@@ -8,16 +8,18 @@
 
 namespace HughCube\Laravel\Octane\Actions;
 
-use HughCube\Laravel\Knight\Routing\Controller;
 use HughCube\Laravel\Octane\Octane;
+use Illuminate\Config\Repository;
+use Illuminate\Container\Container as IlluminateContainer;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Laravel\SerializableClosure\Exceptions\PhpVersionNotSupportedException;
 use Psr\Log\LoggerInterface;
 use Psr\SimpleCache\InvalidArgumentException;
 
-class WaitTaskCompleteAction extends Controller
+class WaitTaskCompleteAction
 {
     /**
      * @return JsonResponse
@@ -39,7 +41,7 @@ class WaitTaskCompleteAction extends Controller
         $message = sprintf('type:%s, uri:%s, workerCount:%s, duration%sms', $type, $uri, $workerCount, $duration);
         $this->getLogChannel()->log($this->getLogLevel(), $message);
 
-        return $this->asJson(['duration' => $duration]);
+        return new JsonResponse(['code' => 200, 'message' => 'ok', 'data' => ['duration' => $duration]]);
     }
 
     /**
@@ -61,5 +63,44 @@ class WaitTaskCompleteAction extends Controller
     protected function getLogLevel(): mixed
     {
         return $this->getContainerConfig()->get('octane.wait_task_complete_log_level', 'debug');
+    }
+
+    /**
+     * @return Repository
+     * @throws BindingResolutionException
+     *
+     */
+    protected function getContainerConfig(): Repository
+    {
+        return $this->getContainer()->make('config');
+    }
+
+    /**
+     * @return Request
+     * @phpstan-ignore-next-line
+     *
+     * @throws
+     */
+    protected function getRequest(): Request
+    {
+        return $this->getContainer()->make('request');
+    }
+
+    /**
+     * @return IlluminateContainer
+     */
+    protected function getContainer(): IlluminateContainer
+    {
+        return IlluminateContainer::getInstance();
+    }
+
+    /**
+     * @throws BindingResolutionException
+     * @throws InvalidArgumentException
+     * @throws PhpVersionNotSupportedException
+     */
+    public function __invoke(): JsonResponse
+    {
+        return $this->action();
     }
 }
